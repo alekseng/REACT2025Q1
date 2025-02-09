0473aeccae40040f0c45ad1338a02c66b2ac2819
alekseng <alexvdche@mail.ru>
2025-02-09 13:05:24 +0300

    feat: add pagination

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,31 +3,42 @@ import { Header } from '../../../widgets/Header';
 import { useEffect, useState } from 'react';
 import { fetchData } from '../../../shared/api/fetchData/fetchData.ts';
 import { Result } from '../../../shared/api/types/types.ts';
-import cls from './MainPage.module.scss';
 import { useLocalStorage } from '../../../shared/lib/hooks/useLocalStorage/useLocalStorage.ts';
 import { Loader } from '../../../shared/ui/Loader/Loader.tsx';
+import { Pagination } from '../../../features/Pagination';
+import { useSearchParams } from 'react-router-dom';
+import cls from './MainPage.module.scss';
 
 export const MainPage = () => {
   const [data, setData] = useState<Result[]>([]);
   const [query, setQuery] = useLocalStorage('aleksengQuery');
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [totalPage, setTotalPage] = useState(0);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const currentPage = Number(searchParams.get('page')) || 1;
 
   useEffect(() => {
-    fetchData(1, query).then((data) => {
+    setLoading(true);
+    fetchData(currentPage, query).then((data) => {
       setData(data.results);
+      setTotalPage(data.total_pages);
       setLoading(false);
     });
-  }, []);
+  }, [currentPage, query]);
 
-  const handleSearch = async (newQuery: string) => {
-    setLoading(true);
+  const handleSearch = (newQuery: string) => {
     setQuery(newQuery);
-    const result = await fetchData(1, newQuery);
-    setData(result.results);
-    setLoading(false);
+    setSearchParams({ page: '1' });
   };
 
-  if (data.length === 0) {
+  const handlePageChange = (newPage: number) => {
+    if (newPage >= 1 && newPage <= totalPage) {
+      setSearchParams({ page: String(newPage) });
+    }
+  };
+
+  if (data.length === 0 && !loading) {
     return (
       <>
         <Header onSearch={handleSearch} />
@@ -41,6 +52,15 @@ export const MainPage = () => {
   return (
     <>
       <Header onSearch={handleSearch} />
+      {loading ? (
+        ''
+      ) : (
+        <Pagination
+          totalPage={totalPage}
+          currentPage={currentPage}
+          onPageChange={handlePageChange}
+        />
+      )}
       {loading ? <Loader /> : <CardList results={data} />}
     </>
   );
