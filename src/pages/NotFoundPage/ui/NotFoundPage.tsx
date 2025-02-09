eb32e71764589d15a542c55f265a61252aa5971f
alekseng <alexvdche@mail.ru>
2025-02-10 01:55:57 +0300

    feat: add test for NotFoundPage

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -3,7 +3,7 @@ import cls from './NotFoundPage.module.scss';
 
 export const NotFoundPage = () => {
   return (
-    <div className={cls['not-found-page']}>
+    <div data-testid="not-found-page" className={cls['not-found-page']}>
       <h3>404</h3>
       <p>page not found</p>
       <Link className={cls.link} to="/">
