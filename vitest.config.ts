ed52b0c37744d9d68efab83175f0e212e8fdfad0
alekseng <alexvdche@mail.ru>
2025-02-09 21:07:20 +0300

    feat: add vitest. add test for Button

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/shared/config/vitest/setupTests.ts',
+    coverage: {
+      provider: 'istanbul',
+      include: ['**/*.tsx'],
+      exclude: [
+        '**/node_modules/**',
+        '**/*.test.tsx',
+        '**/*.spec.tsx',
+        'src/__tests__/setup.ts',
+      ],
+    },
+  },
+});
